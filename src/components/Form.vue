c8a471893e7e26fe19c85eeb353a0804b9f0a018
programm_(some kind of it) <vasilenkoa0101@gmail.com>
2021-07-23 09:28:21 +0300

    Group component accepts slots

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,17 +1,11 @@
 <template>
-    <div>
-        <Group
-            v-on:click = "toggleFormVisibility"
-            v-bind:title = "serverResponse.title"
+    <Group v-bind:title = "serverResponse.title">
+        <BoldString
+            v-for = "arrElement in serverResponse.values"
+            v-bind:value = "arrElement"
+            v-bind:key = "arrElement.values"
         />
-        <div v-if = "isVisible">
-            <BoldString
-                v-for = "arrElement in serverResponse.values"
-                v-bind:value = "arrElement"
-                v-bind:key = "arrElement.values"
-            />
-        </div>        
-    </div>
+    </Group>
 </template>
 
 <script>
@@ -19,32 +13,16 @@ import Group from "@/components/FormElements/Group"
 import BoldString from "@/components/FormElements/BoldString"
 
 export default{
-    data () {
-        return {
-            isVisible: true
-        }
-    },
     props: ['serverResponse'],
     components: {
         Group:Group,
         BoldString:BoldString
-    },
-    methods: {
-        toggleFormVisibility() {
-            this.isVisible = !this.isVisible
-        }
-    }
+    }    
 }
 </script>
 
 
 <style scoped>
-    div {
-        display: block;
-        background-color: rgb(189, 224, 238);
-        margin-top: 1%;
-    }
-
     strong {
         margin: 1%;
     }
--- src/components/FormElements/Group.vue
+++ src/components/FormElements/Group.vue
@@ -1,15 +1,34 @@
 <template>
-    <h3>{{ title }}</h3>
+    <h3 v-on:click = "toggleFormVisibility">{{ title }}</h3>
+    <div>
+        <slot v-if="isVisible"></slot>
+    </div>
 </template>
 
 <script>
-export default {    
+export default {  
+    data () {
+        return {
+            isVisible: true
+        }
+    },  
     props: ['title'],
+    methods: {
+        toggleFormVisibility() {
+            this.isVisible = !this.isVisible
+        }
+    }
 }
 </script>
 
 <style scoped>
     h3 {
         background-color: white;
+    }
+
+    div {
+        display: block;
+        background-color: rgb(189, 224, 238);
+        margin-top: 1%;
     }
 </style>
